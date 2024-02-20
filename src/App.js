7d07c9a609a963a347be8ba89fcbe3c565ec07d1
arafat-rifat <arafat15-12603@diu.edu.bd>
2024-02-21 03:37:14 +0600

    Compliting My Assaignment

--- src/App.js
+++ src/App.js
@@ -12,9 +12,12 @@ function App() {
   );
 }
 
-// This Is For Step
-const Steps = () => {
-  const [step, setStep] = useState(0);
+
+// This Is For Counter
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  const [step, setStep] = useState(1);
 
   const handleMinusStep = function () {
     if (step > 0) setStep((currentStep) => currentStep - 1);
@@ -24,8 +27,22 @@ const Steps = () => {
     if (step < 10) setStep((currentStep) => currentStep + 1);
   };
 
+  const handleMinusCount = function () {
+    setCount((currentCount) => currentCount - step);
+  };
+
+  const handlePlusCount = function () {
+    setCount((currentCount) => currentCount + step);
+  };
+
+  const date = new Date();
+
+  date.setDate(date.getDate() + count);
+
   return (
-    <div className="flex items-center gap-3">
+    <div className="flex flex-col items-center gap-3">
+       {/* Steps */}
+       <div className="flex items-center gap-3">
       <button
         className="px-2 font-bold border border-solid border-black text-3xl"
         onClick={handleMinusStep}
@@ -41,28 +58,9 @@ const Steps = () => {
       >
         +
       </button>
-    </div>
-  );
-};
-
-// This Is For Counter
-const Counter = () => {
-  const [count, setCount] = useState(0);
-
-  const handleMinusCount = function () {
-    setCount((currentCount) => currentCount - 1);
-  };
-
-  const handlePlusCount = function () {
-    setCount((currentCount) => currentCount + 1);
-  };
-
-  const date = new Date();
-
-  date.setDate(date.getDate() + count);
-
-  return (
-    <div className="flex flex-col items-center gap-3">
+       </div>
+       
+       {/* Count */}
       <div className="flex items-center gap-3">
         <button
           className="px-2 font-bold border border-solid border-black text-3xl"
@@ -80,7 +78,8 @@ const Counter = () => {
           +
         </button>
       </div>
-
+        
+        {/* Date */}
        <div>
         <span className="font-bold text-2xl">
           {count === 0 ? "Today is " : count > 0 ?`${count} days from today is ` : `${Math.abs(count)} days ago was `}
